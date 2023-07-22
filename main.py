dade4e2b893b3c8eb01e603b9f20656dae046118
ndifah04 <105841116322@student.unismuh.ac.id>
2023-07-21 18:48:42 -0700

    py

---
+++ main.py
@@ -0,0 +1,164 @@
+# print ("apa kabar")
+# data1 = 10
+# data2 = 20
+# hasil = data1 + data2
+# print("nilai data1",data1 ,"dan nilai data2", data2 ,"hasil dari data 1 dan 2=", hasil)
+
+# nilai1 = 11
+# nilai2 = 1
+# hasil = nilai1 + nilai2
+# print("coconut angkatan", hasil)
+
+# data1= (int(input("masukan nilai 1=")))
+# data2= (int(input("masukkan nilai 2=")))
+# hasil= data1 + data2
+# print("hasil=",hasil)
+
+# data1= (float(input("masukan nilai 1=")))
+# data2= (float(input("masukkan nilai 2=")))
+# hasil= data1 + data2
+# print("hasil=",hasil)
+
+# data1= (int(input("")))
+# print("angkatan",data1)
+
+# data1= (int(input("")))
+
+# print("    *****   *****      ")
+# print("   ***************      ")
+# print("  *****************     ")
+# print("   ****coconut*****     ")
+# print("    ***angkatan***      ")
+# print("     ***",data1,"****   ")
+# print("      *********         ")
+# print("       *******          ")
+# print("        *****           ")
+# print("         ***              ")
+# print("angkatan",data1)
+
+
+
+# alas=(int(input("masukkan nilai alas=")))
+# tinggi=(int(input("masukkan nilai tinggi=")))
+# hasil= alas*tinggi*0.5
+# print(hasil)
+
+# panjang= int(input("masukkan nilai alas="))
+# lebar= int(input("masukkan nilai lebar="))
+# hasil= panjang*lebar
+# print(hasil)
+
+# p= 10*("*")
+# j= 3*("*")
+# print(p)
+# print(p)
+# print(j)
+# print(j)
+# print(p)
+# print(p)
+# print(j)
+# print(j)
+# print(j)
+# print(j)
+
+# a= 0
+# print(a)
+# a= a+1
+# print(a, " ", a)
+# a=a*2
+# print(a," ",a," ",a)
+# a=a*3
+# print(a," ",a," ",a," ",a)
+# a=a*4
+# print(a," ",a," ",a," ",a," ",a)
+# a=a-120
+# print(a," ",a," ",a," ",a," ",a," ",a)
+
+# lulus= input("apakah kmu lulus? [ya/]tidak]:")
+# if lulus == "tidak":
+#     print("kamu harus ikut ujian")
+
+# a=1*("#")
+# b=2*("#")
+# c=3*("#")
+# d=4*("#")
+# e=5*("#")
+# print(a,a,a)
+# print(b,b,b)
+# print(c,c,c)
+# print(d,d,d)
+# print(e,e,e)
+
+# lulus= input("apakah kmu lulus? [ya/]tidak]:")
+# if lulus == "tidak":
+#     print("semangat dan jangan putus asa")
+# if lulus == "ya":
+#     print("selamat anda lulus")
+
+# lulus = str(input("apakah kamu gay ?"))
+# if lulus == "yes":
+#     print("selamat anda mendapatkan tiket ke jerman")
+# if lulus == "no":
+#     print("anda harus menghubungi seseorang atas nama Ragilll")
+
+# a=(int(input("masukkan nilai a=")))
+# b=(int(input("masukkan nilai b=")))
+# c=a*b
+# print(c+1)
+
+# a=1
+# b=2
+# c=3
+# d=4
+# e=5
+# print(a," ",a)
+# print(b," ",a," ",b)
+# print(c," ",a," ",b," ",c)
+# print(d," ",a," ",b," ",c," ",d)
+# print(e," ",a," ",b," ",c," ",d," ",e)
+
+# print("    DDDD    DDDD")
+# print("   DDDDDD  DDDDDD")
+# print("   DDDDDDDDDDDDDD")
+# print("    DDDDDDDDDDDD")
+# print("     DDDDDDDDDD")
+# print("      DDDDDDDD")
+# print("       DDDDDD")
+# print("        DDDD")
+# print("         DD")
+
+
+# lulus = str(input("1+1=?"))
+# if lulus == "2":
+#     print("jawaban anda benar")
+# if lulus == "3":
+#     print("jawaban anda salah")
+
+
+# a = (int(input("")))
+# b = (int(input("")))
+# hasil = a + b
+# print( hasil)
+
+# a= (int(input("masukkan nilai awal=")))
+# b= (int(input("masukkan nialai yang akan dijumlahkan dengan nilai awal=")))
+# hasil= a+b
+# print("penjumlahannya adalah",a ,"+", b, "=",hasil) 
+
+a1= (int(input("masukkan nilai alas 1=")))
+t1= (int(input("masukkan nilai tinggi 2=")))
+a2= (int(input("masukkan nilai alas 2=")))
+t2= (int(input("masukkan nilai tinggi 2=")))
+h1= 1/2 * t1*a1
+h2= 1/2 * t2*a2
+if h1 %2==0:
+    print("luas segi tiga 1=", h1, "genap")
+else:
+    print("luas segi tiga 1=", h1 ,"ganjil")
+if h2 %2==0:
+    print("luas segitiga 2=", h2, "genap")
+else:
+    print("luas segi tiga 2=", h2,"ganjil")
+
+
+
---
+++ tes.py
@@ -0,0 +1,8 @@
+print("YUK MENENTUKAN BILANGAN GANJIL DAN GENAP")
+a=int(input("masukkan bilangan yang ingin kamu ketahui dia ganjil apa genap sih? \nmasukkan bilangan: "))
+if a %2==0:
+    print(a,"adalah bilangan genap")
+else:
+    print(a,"adalah bilangan ganjil")
+
+    b=str(input("apakah anda ingin coba lagi? y/n"))
